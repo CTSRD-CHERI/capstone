2640de7c0b63746fbed4f6da652ca55ffd706d6f
Mingle Chen <minglechen2002@gmail.com>
2024-08-09 19:32:55 +0100

    Extract Morello CSysRegs from system operands

--- suite/synctools/systemoperand.py
+++ suite/synctools/systemoperand.py
@@ -791,6 +791,71 @@ print_line("""
 }
 """)
 
+# extract MorelloCSysRegsList
+count = 0
+c = 0
+for line in lines:
+    line = line.rstrip()
+
+    if len(line.strip()) == 0:
+        continue
+
+    if line.strip() == 'constexpr MorelloCSysReg MorelloCSysRegsList[] = {':
+        count += 1
+        print_line('static const MorelloCSysReg MorelloCSysRegsList[] = {')
+        continue
+
+    line = line.strip()
+    if count == 1:
+        if line == '};':
+            # done with first enum
+            print_line('};\n')
+            break
+        else:
+            # skip pseudo instructions
+            line = line.replace('::', '_')
+            #line = line.replace('{}', '{ 0 }')
+            line = line.replace('{}', '')
+            tmp = line.split(',')
+            print_line("  %s, %s }, // %u" %(tmp[0].lower(), tmp[1], c))
+            #print_line("  %s" %line.lower())
+            c += 1
+
+# lookupMorelloCSysRegByEncoding
+count = 0
+for line in lines:
+    line = line.rstrip()
+
+    if len(line.strip()) == 0:
+        continue
+
+    if 'lookupMorelloCSysRegByEncoding' in line and '{' in line:
+        count += 1
+        print_line('const MorelloCSysReg *lookupMorelloCSysRegByEncoding(uint16_t Encoding)\n{')
+        print_line('  unsigned int i;')
+        continue
+
+    if count == 1 and 'IndexType Index[] = {' in line:
+        count += 1
+
+    if count == 2:
+        if line.strip() == '};':
+            # done with array, or this function?
+            print_line(line)
+            break
+        else:
+            # enum items
+            print_line(line)
+
+print_line("""
+  i = binsearch_IndexTypeEncoding(Index, ARR_SIZE(Index), Encoding);
+  if (i == -1)
+    return NULL;
+  else
+    return &MorelloCSysRegsList[Index[i].index];
+}
+""")
+
 # extract PStatesList
 count = 0
 c = 0
